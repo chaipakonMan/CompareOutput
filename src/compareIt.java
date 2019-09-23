135d3676b2af1101c69ed95be4f9a990221f3772
Chaipakon Luangprasert <chaipakon197@vt.edu>
2019-09-23 16:04:15 -0500

    Free time... DONE

--- src/compareIt.java
+++ src/compareIt.java
@@ -1,3 +1,7 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
 /**
  * compreIt class is a personal project to make my life easier.
  * help me dealing with grading my school assignment.
@@ -8,10 +12,39 @@
 public class compareIt {
 
     public static void main(String[] args) {
-        
-        
+        compareIt(args[0], args[1]);
+        // System.out.println(args[0] + " " + args[1]);
+
     }
-    
-    
+
+
+    public static void compareIt(String myOut, String profOut) {
+        // read in
+        try {
+            File fil = new File(myOut);
+            File fils = new File(profOut);
+
+            Scanner scanner = new Scanner(fil);
+            Scanner scanners = new Scanner(fils);
+
+            while (scanner.hasNextLine()) {
+
+                String line = scanner.nextLine().toLowerCase().trim();
+                String lines = scanners.nextLine().toLowerCase().trim();
+
+                if (line.equals(lines)) {
+                    System.out.println("Good!");
+                }
+                else {
+                    System.out.println("No Good!");
+                }
+
+            }
+            scanner.close();
+        }
+        catch (FileNotFoundException e) {
+            System.out.println("can't read in the given file!");
+        }
+    }
 
 }
