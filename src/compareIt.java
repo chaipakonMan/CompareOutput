283022b981bd4f8d1b61658ace405e0ef515627b
Chaipakon Luangprasert <chaipakon197@vt.edu>
2019-09-23 15:33:55 -0500

    first commit

---
+++ src/compareIt.java
@@ -0,0 +1,17 @@
+/**
+ * compreIt class is a personal project to make my life easier.
+ * help me dealing with grading my school assignment.
+ * 
+ * @author Chaipakon Luangprasert
+ * @version <September 2019>
+ */
+public class compareIt {
+
+    public static void main(String[] args) {
+        
+        
+    }
+    
+    
+
+}
